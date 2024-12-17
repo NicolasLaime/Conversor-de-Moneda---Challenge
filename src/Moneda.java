31e7242ea0c472e60359440f2b5417e6d3beb013
NicolasLaime <nicolaime100@gmail..com>
2024-12-17 14:42:31 -0300

    arreglamos detalles

--- src/Moneda.java
+++ src/Moneda.java
@@ -1,4 +1,65 @@
-package PACKAGE_NAME;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Moneda {
+
+    private static final String API_KEY = "cb902b2319e27ad21ef5fd1b";
+    private static final String BASE_URL = "https://v6.exchangerate-api.com/v6/";
+
+    public static String obtenerCodigoDeMoneda(int opcion){
+        return switch (opcion){
+            case 1 -> "ARS";
+            case 2 -> "BOB";
+            case 3 -> "BRL";
+            case 4 -> "CLP";
+            case 5 -> "COP";
+            case 6 -> "USD";
+            default -> "USD";
+
+
+        };
+    }
+
+
+    public  static double obtenerTasaDeCambio(String monedaOrigen , String monedaDestino){
+        try {
+            String urlStr = BASE_URL + API_KEY + "/latest/" + monedaOrigen;
+            URL url = new URL(urlStr);
+
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line= in.readLine()) != null){
+                response.append(line);
+            }
+            in.close();
+
+            Gson gson = new Gson();
+            JsonObject jsonResponse = gson.fromJson(response.toString(), JsonObject.class);
+
+
+            JsonObject rates = jsonResponse.getAsJsonObject("conversion_rates");
+            return rates.get(monedaDestino).getAsDouble();
+
+
+
+        } catch (Exception e){
+            System.out.println("Error en la aplicacion" + e.getMessage());
+            return -1;
+        }
+    }
+
+
+
+
+
+
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,59 @@
-package PACKAGE_NAME;
+// Clase principal
+import java.util.Scanner;
 
 public class Principal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("--------------------------------------------------------");
+            System.out.println("Bienvenido al convertidor de monedas - Challenge ONE ");
+            System.out.println("--------------------------------------------------------");
+
+            System.out.println("Elige la moneda de origen:");
+            System.out.println("1. ARS - Peso argentino");
+            System.out.println("2. BOB - Boliviano boliviano");
+            System.out.println("3. BRL - Real brasileño");
+            System.out.println("4. CLP - Peso chileno");
+            System.out.println("5. COP - Peso colombiano");
+            System.out.println("6. USD - Dólar estadounidense");
+
+            //Capturamos la moneda de origen
+            String monedaOrigen = Moneda.obtenerCodigoDeMoneda(scanner.nextInt());
+
+            System.out.println("Elige la moneda de destino:");
+            String monedaDestino = Moneda.obtenerCodigoDeMoneda(scanner.nextInt());
+
+            System.out.println("Ingresa el monto a convertir:");
+            double monto = scanner.nextDouble();
+
+            //método para obtener la tasa de cambio
+            double tasaCambio = Moneda.obtenerTasaDeCambio(monedaOrigen, monedaDestino);
+
+            if (tasaCambio != -1) {
+                // Calculamos el monto convertido
+                double montoConvertido = monto * tasaCambio;
+                System.out.println("\nResultado:");
+                System.out.println("--------------------------------------------------------");
+                System.out.printf("%.2f %s equivalen a %.2f %s\n", monto, monedaOrigen, montoConvertido, monedaDestino);
+                System.out.println("--------------------------------------------------------");
+
+            } else {
+                System.out.println("No se pudo obtener la tasa de cambio. Inténtalo nuevamente.");
+            }
+
+            System.out.println("\n¿Deseas realizar otra conversión?");
+            System.out.println("1. Sí");
+            System.out.println("0. No (Salir)");
+            opcion = scanner.nextInt();
+
+        } while (opcion != 0);
+
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Gracias por usar el convertidor de monedas. ¡Hasta pronto!");
+        System.out.println("--------------------------------------------------------");
+
+        scanner.close();
+    }
 }
